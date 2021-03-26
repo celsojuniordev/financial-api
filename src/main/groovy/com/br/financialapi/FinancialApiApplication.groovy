fa24a2f6d5bd4ee20d1e7cfbbfbf8f34b6006fc5
Celso Junior <celsoestercio.jr@gmail.com>
2021-03-25 21:53:42 -0300

    first commit

---
+++ src/main/groovy/com/br/financialapi/FinancialApiApplication.groovy
@@ -0,0 +1,13 @@
+package com.br.financialapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FinancialApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FinancialApiApplication, args)
+	}
+
+}

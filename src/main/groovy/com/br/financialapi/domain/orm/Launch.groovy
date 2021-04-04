fb653f81b9df3407062b4f2017f8fb8e520051dc
Celso Junior <celsoestercio.jr@gmail.com>
2021-04-03 22:21:05 -0300

    feat: create entity to launch

--- src/main/groovy/com/br/financialapi/domain/orm/Launch.groovy
+++ src/main/groovy/com/br/financialapi/domain/orm/Launch.groovy
@@ -2,13 +2,16 @@ package com.br.financialapi.domain.orm
 
 import com.br.financialapi.domain.enums.LaunchStatus
 import com.br.financialapi.domain.enums.LaunchType
+import lombok.Builder
+import lombok.Data
 import lombok.Getter
 import lombok.Setter
 
 import javax.persistence.*
 
-@Getter @Setter
 @Entity
+@Data
+@Builder
 class Launch {
 
     @Id

c5b68e32f52d97049d17998efab09d79d79b73c6
Celso Junior <celsoestercio.jr@gmail.com>
2021-05-31 23:11:33 -0300

    chore: add cors config

--- src/main/groovy/com/br/financialapi/FinancialApiApplication.groovy
+++ src/main/groovy/com/br/financialapi/FinancialApiApplication.groovy
@@ -2,12 +2,20 @@ package com.br.financialapi
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
-class FinancialApiApplication {
+@EnableWebMvc
+class FinancialApiApplication implements WebMvcConfigurer {
 
 	static void main(String[] args) {
 		SpringApplication.run(FinancialApiApplication, args)
 	}
 
+	@Override
+	void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
+	}
 }
